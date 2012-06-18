2cb21d0bf0572badd453f3a0b0e800aef3e0d5ae
mark.goadrich <mark.goadrich@4b57915e-7032-37f9-f217-544fae6445e4>
2012-06-18 16:47:45 +0000

    Demo app for NeuroSky SDK

---
+++ trunk/TestingEEG/src/com/googlecode/neuraid/testingeeg/TestingEEGActivity.java
@@ -0,0 +1,13 @@
+package com.googlecode.neuraid.testingeeg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestingEEGActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
